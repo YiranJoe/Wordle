ea845f905513a75463d1a144d81456bdc42815d0
hujinluo <hujinluo@localhost>
2021-09-21 09:21:10 -0400

    test

---
+++ IntroUnit/src/introUnit/Java.java
@@ -0,0 +1,12 @@
+package introUnit;
+
+public class Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("What Sport Do I Play? I Play Soccer.");
+		System.out.println("Where Am I From? I Am From China.");
+		System.out.println("When Is My Birthday? 3 days later.");
+	}
+
+}
