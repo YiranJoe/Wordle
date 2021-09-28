91f3e052530f98457ce28998e5a2ceaf00b8ac45
yiranhu <yiranhu@localhost>
2021-09-28 19:48:53 -0400

    HW928 Take me 3 hours

--- Test/src/ScannerProject.java
+++ Test/src/ScannerProject.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
 public class ScannerProject {
 	
-	public static void num() 
-	{
 
-	}
-	
-	public static void main(String[] ars)
+	/*public static void main(String[] ars)//print 4 inverse number
 	{
 		Scanner num=new Scanner(System.in);
 		System.out.print("enter your numbers");
@@ -16,4 +12,89 @@ public class ScannerProject {
 		int num4=num.nextInt();
 		System.out.println(num1+num2*10+num3*100+num4*1000);
 	}
-}
+}*/
+
+	/*public static void main(String[] ars)//one string and 2 numbers
+	{
+		Scanner num=new Scanner(System.in);
+		System.out.print("enter your string");
+		String string1=num.nextLine();
+		System.out.print("enter 2 numbers");
+		int num1=num.nextInt();
+		int num2=num.nextInt();
+		if(string1.equals("Plus"))
+		{
+			System.out.println(num1+num2);
+		}
+		else if(string1.equals("Minus"))
+		{
+			System.out.println(Math.abs(num1-num2));
+		}
+		else if(string1.equals("Divide"))
+		{
+			System.out.println((double)(num1/num2));
+		}
+		else if(string1.equals("Multiply"))
+		{
+			System.out.println(num1*num2);
+		}
+		else if(string1.equals("Modulate"))
+		{
+			System.out.println(num1%num2);
+		}
+		else
+		{
+			System.out.println(string1 + (char)(num1+num2));
+		}
+	}
+}*/
+	
+	
+	
+	
+	
+	/*public static int x=0,y=0;
+	
+	public static void main(String[] ars)
+	{
+		Scanner num=new Scanner(System.in);
+		System.out.print("Enter your touchdowns,field goals,score: ");
+		int touch=num.nextInt();
+		int field=num.nextInt();
+		int score=num.nextInt();
+		for(int i = 0; i < touch; i++) {
+			for(int j = 0; j < field; j++) {
+				if(i*7+j*3==score) 
+				{
+					System.out.println("YES");
+					x=1;
+					break;
+				}
+			}
+		}
+		if(x==0)
+		{
+			System.out.println("NO");
+		}
+	}
+}*/
+	
+	
+	
+	/*public static void main(String[] ars)
+	{
+		int max=0;
+		Scanner num=new Scanner(System.in);
+		System.out.print("Enter your number here: ");
+		int num1=num.nextInt();
+		for(int i=0;i<3;i++) 
+		{
+		if(num1%10>max)
+		{
+			max=num1%10;
+		}
+		num1=num1/10;
+		}
+		System.out.println(max);
+	}
+*/}
