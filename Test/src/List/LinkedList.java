171e54350d61b491ba35cdd29be53741b7c3f034
YiranJoe <90701701+YiranJoe@users.noreply.github.com>
2022-04-05 20:54:27 -0400

    .

---
+++ Test/src/List/LinkedList.java
@@ -0,0 +1,83 @@
+package List;
+
+public class LinkedList<Q> {
+
+	private class Node{
+		
+		Q info;
+		Node next;
+		
+		public Node(Q i,Node n) {
+			info=i;
+			next=n;
+		}
+	}
+	
+	private Node head=null;
+	
+	public void add(Q info) {
+		Node newNode=new Node(info,null);
+		if(head==null) {
+			head=newNode; 
+		}else {
+			Node curr=head;
+			while(curr.next!=null) {
+				curr=curr.next;
+			}
+			curr.next=newNode;
+		}
+	}
+	
+	public void add(Q info, int index) {
+		Node newNode=new Node(info,null);
+		Node curr=head;
+		if(index==0) {
+			Node curr2=head;
+			head=newNode;
+			newNode.next=curr2;
+		}else {
+			for(int i=0;i<index-1;i++) {
+				curr=curr.next;
+			}
+			Node curr2=curr.next;
+			curr.next=newNode;
+			newNode.next=curr2;
+		}
+	}
+	
+	public void remove(int index) {
+		Node curr=head;
+		if(index==0) {
+			head=head.next;;
+		}else {
+			for(int i=0;i<index-1;i++) {
+				curr=curr.next;
+			}
+			Node curr2=curr.next.next;
+			curr.next=curr2;
+		}
+	}
+	
+	public Q get(int i) {
+		try {
+			Node curr=head;
+			for(int j=0;j<i;j++) {
+				curr=curr.next;
+			}
+			return curr.info;
+		}catch(NullPointerException e) {
+			throw new IndexOutOfBoundsException();
+		}
+	}
+	
+	public String toString() {
+		String a="";
+		Node curr=head;
+		while(curr!=null) {
+			a+=curr.info;
+			curr=curr.next;
+		}
+		return a;
+	}
+	
+}
---
+++ Test/src/List/LinkedList_Test.java
@@ -0,0 +1,25 @@
+package List;
+
+
+public class LinkedList_Test<Q> {
+
+	
+	public static void main(String[] args) {
+		LinkedList<Integer> list=new LinkedList<Integer>();
+		for(int i=0;i<8;i++) {
+			list.add(i);
+		}//list should be 01234567
+		System.out.println(list.toString());
+		list.add(9);//list should be 012345679
+		System.out.println(list.toString());
+		System.out.println(list.get(8));//print 9 since it is the content of index 9
+		list.add(3,5);//list should be 0123435679
+		System.out.println(list.toString());
+		list.add(1,0);//list should be 10123435679
+		System.out.println(list.toString());
+		list.remove(0);//list should be 0123435679
+		System.out.println(list.toString());
+		list.remove(2);//list should be 0131435679
+		System.out.println(list.toString());
+	}
+}
