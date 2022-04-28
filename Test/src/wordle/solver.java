4ad321817e7d6907131f158baf24279364ee450f
YiranJoe <90701701+YiranJoe@users.noreply.github.com>
2022-04-28 19:45:30 -0400

    WordleSolver

--- Test/src/wordle/solver.java
+++ Test/src/wordle/solver.java
@@ -8,17 +8,20 @@ public class solver {
 	public static Scanner io=new Scanner(System.in);
 	public static ArrayList<String> words=new ArrayList<>();
 	public static HashMap<Integer,Character> sure=new HashMap<>();
+	public static HashMap<Integer,Character> unsure=new HashMap<>();
 	
 	
 	public static void analysis(String word,ArrayList<String> list) {
 		int length=word.length();
-		System.out.println("input the hint:");
+		System.out.print("input the hint:");
 		for(int i=0;i<length;i++) {
 			char a=io.next().charAt(0);
 			if(a=='-') {
 				ban.add(word.charAt(i));
+				unsure.put(i, word.charAt(i));
 			}else if(a=='x') {
 				contain.add(word.charAt(i));
+				unsure.put(i, word.charAt(i));
 			}else {
 				sure.put(i, word.charAt(i));
 			}
@@ -27,11 +30,29 @@ public class solver {
 			if(contain.indexOf(sure.get(index))==-1) {
 				contain.add(sure.get(index));
 			}
+			for(int i=0;i<list.size();i++) {
+				if(list.get(i).charAt(index)!=sure.get(index)) {
+					list.remove(i);
+					i--;
+				}
+			}
 		}
 		for(int i=0;i<contain.size();i++) {
 			for(int j=0;j<ban.size();j++) {
 				if(ban.get(j)==contain.get(i)) {
 					ban.remove(j);
+					j--;
+				}
+			}
+			for(int j=0;j<list.size();j++) {
+				if(list.get(j).indexOf(contain.get(i))==-1) {
+					list.remove(j);
+					j--;
+				}
+			}
+			for(int j=0;j<list.size();j++) {
+				if(list.get(j).indexOf(contain.get(i))==word.indexOf(contain.get(i))&&!sure.containsValue(contain.get(i))) {
+					list.remove(j);
 					j--;
 				}
 			}
@@ -39,15 +60,6 @@ public class solver {
 //		System.out.println(ban);
 //		System.out.println(contain);
 //		System.out.println(sure);
-		for(int i=0;i<contain.size();i++) {
-			for(int j=0;j<ban.size();j++) {
-				if(ban.get(j)==contain.get(i)) {
-					ban.remove(j);
-					j--;
-				}
-			}
-		}
-
 		for(int i=0;i<ban.size();i++) {
 			for(int j=0;j<list.size();j++) {
 				if(list.get(j).indexOf(ban.get(i))!=-1) {
@@ -57,31 +69,32 @@ public class solver {
 			}
 		}
 		
-		for(int i=0;i<contain.size();i++) {
-			for(int j=0;j<list.size();j++) {
-				if(list.get(j).indexOf(contain.get(i))==-1) {
-					list.remove(j);
-					j--;
-				}
-			}
-		}
+//		for(int i=0;i<contain.size();i++) {
+//			for(int j=0;j<list.size();j++) {
+//				if(list.get(j).indexOf(contain.get(i))==-1) {
+//					list.remove(j);
+//					j--;
+//				}
+//			}
+//		}
 		
-		for(int index:sure.keySet()) {
+//		for(int index:sure.keySet()) {
+//			for(int i=0;i<list.size();i++) {
+//				if(list.get(i).charAt(index)!=sure.get(index)) {
+//					list.remove(i);
+//					i--;
+//				}
+//			}
+//		}
+		for(int index:unsure.keySet()) {
 			for(int i=0;i<list.size();i++) {
-				if(list.get(i).charAt(index)!=sure.get(index)) {
+				if(list.get(i).charAt(index)==unsure.get(index)) {
 					list.remove(i);
 					i--;
 				}
 			}
 		}
-		for(int i=0;i<contain.size();i++) {
-			for(int j=0;j<list.size();j++) {
-				if(list.get(j).indexOf(contain.get(i))==word.indexOf(contain.get(i))&&!sure.containsValue(contain.get(i))) {
-					list.remove(j);
-					j--;
-				}
-			}
-		}
+//	
 		
 		System.out.println(list);
 	}
@@ -94,7 +107,7 @@ public class solver {
 			a=in.readLine();
 		}
 		while(true) {
-			System.out.println("input your try:");
+			System.out.print("input your try:");
 			String w=io.next();
 			analysis(w,words);
 		}
