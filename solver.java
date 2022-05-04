df13b6fd374d34259b79ec438210094eec5d781a
YiranJoe <90701701+YiranJoe@users.noreply.github.com>
2022-05-04 09:03:36 -0400

    Add files via upload

--- solver.java
+++ solver.java
@@ -15,26 +15,29 @@ public class solver {
 	 * repeat 1-4
 	 * GOOD LUCK WITH WORDLE!
 	 */
-	
+	public static int len=5;
 	public static ArrayList<Character> ban=new ArrayList<>();
 	public static ArrayList<Character> contain=new ArrayList<>();
 	public static Scanner io=new Scanner(System.in);
 	public static ArrayList<String> words=new ArrayList<>();
+	public static ArrayList<String> vocab=new ArrayList<>();
 	public static HashMap<Integer,Character> sure=new HashMap<>();
-	public static HashMap<Integer,Character> unsure=new HashMap<>();
+//	public static HashMap<Integer,Character> unsure=new HashMap<>();
+	public static ArrayList<Character>[] unsure=new ArrayList[len];
+	public static boolean delima=false;
 	
 	
 	public static void analysis(String word,ArrayList<String> list) {
-		int length=word.length();
-		System.out.print("input the hint:");
+		int length=len;
+		System.out.println("input the hint:");
 		for(int i=0;i<length;i++) {
 			char a=io.next().charAt(0);
 			if(a=='-') {
 				ban.add(word.charAt(i));
-				unsure.put(i, word.charAt(i));
+				unsure[i].add(word.charAt(i));
 			}else if(a=='x') {
 				contain.add(word.charAt(i));
-				unsure.put(i, word.charAt(i));
+				unsure[i].add(word.charAt(i));
 			}else {
 				sure.put(i, word.charAt(i));
 			}
@@ -82,30 +85,143 @@ public class solver {
 			}
 		}
 		
-		for(int index:unsure.keySet()) {
-			for(int i=0;i<list.size();i++) {
-				if(list.get(i).charAt(index)==unsure.get(index)) {
-					list.remove(i);
-					i--;
+//		for(int index:unsure) {
+//			for(int i=0;i<list.size();i++) {
+//				if(list.get(i).charAt(index)==unsure.get(index)) {
+//					list.remove(i);
+//					i--;
+//				}
+//			}
+//		}
+		for(int i=0;i<unsure.length;i++) {
+			for(int k=0;k<unsure[i].size();k++) {
+				for(int j=0;j<list.size();j++) {
+					if(list.get(j).charAt(i)==unsure[i].get(k)) {
+						list.remove(j);
+						j--;
+					}
 				}
 			}
 		}
+//		System.out.println(Arrays.toString(unsure));
+		if(sure.size()>=2) {
+			delima=true;
+		}
 
-		
+		System.out.println("Choices:");
 		System.out.println(list);
 	}
 	
+	public static boolean single(String word) {
+		for(int i=0;i<word.length()-1;i++) {
+			for(int j=i+1;j<word.length();j++) {
+				if(word.charAt(i)==word.charAt(j)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static ArrayList<Character> mostCommonLetters(ArrayList<String> remainingWords) {
+//		ArrayList<Character> list=new ArrayList<>();
+		ArrayList<Integer> unsureDigit=new ArrayList<>();
+		ArrayList<Character> returnList=new ArrayList<>();
+		PriorityQueue<Integer> numlist=new PriorityQueue<>();
+		HashMap<Character,Integer> list=new HashMap<>();
+		for(int i=0;i<len;i++) {
+			if(!sure.containsKey(i)) {
+				unsureDigit.add(i);
+			}
+		}
+		for(int i=0;i<remainingWords.size();i++) {
+			for(int j=0;j<unsureDigit.size();j++) {
+				if(list.containsKey(remainingWords.get(i).charAt(unsureDigit.get(j)))) {
+					list.put(remainingWords.get(i).charAt(unsureDigit.get(j)),1+list.get(remainingWords.get(i).charAt(unsureDigit.get(j))));
+				}else {
+					list.put(remainingWords.get(i).charAt(unsureDigit.get(j)), 0);
+				}
+			}
+		}
+//		System.out.println(list);
+		for(char Character:list.keySet()) {
+				numlist.add(list.get(Character));
+		}
+		while(numlist.size()>3) {
+			numlist.poll();
+		}
+		for(int n:numlist) {
+			for(char c:list.keySet()) {
+				if(list.get(c)==n&&!returnList.contains(c)) {
+					returnList.add(c);
+					break;
+				}
+			}
+		}
+//		System.out.println(numlist);
+		return returnList;
+	}
+	
+	public static boolean isBest(String word) {
+		for(int i=0;i<unsure.length;i++) {
+			if(unsure[i].contains(word.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static ArrayList<String> best(ArrayList<String> listOfWords,ArrayList<String> remainingWords){
+		ArrayList<String> returnList=new ArrayList<>();
+		for(int i=0;i<listOfWords.size();i++) {
+			if(containLetter(listOfWords.get(i),mostCommonLetters(remainingWords))&&isBest(listOfWords.get(i))) {
+				returnList.add(listOfWords.get(i));
+			}
+		}
+		System.out.println("Common letters:");
+		System.out.println(mostCommonLetters(remainingWords));
+		System.out.println("Suggest you to choose:");
+		return returnList;
+	}
+	
+	public static boolean containLetter(String words, ArrayList<Character> list) {
+		int count=0;
+		if(!single(words)) {
+			return false;
+		}
+		for(int i=0;i<words.length();i++) {
+				for(int j=0;j<list.size();j++) {
+					if(words.charAt(i)==list.get(j)) {
+						count++;
+						break;
+					}
+				}
+		}
+		if(count>=list.size()) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
 	public static void main(String[] args) throws Exception{
+		for(int i=0;i<len;i++) {
+			unsure[i]=new ArrayList<Character>();
+		}
 		BufferedReader in=new BufferedReader(new FileReader("answer"));
 		String a=in.readLine();
 		while(a!=null) {
 			words.add(a);
+			vocab.add(a);
 			a=in.readLine();
 		}
 		while(true) {
-			System.out.print("input your try:");
+			System.out.println("input your try:");
 			String w=io.next();
 			analysis(w,words);
+			if(delima==true&&words.size()>=3) {
+				System.out.println(best(vocab,words));
+			}
 		}
 	}
 }
